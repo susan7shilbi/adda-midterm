5cedfb38db890b83d1d94748fadc15d5eedefe74
Susan Shilbi <76254590+susan7shilbi@users.noreply.github.com>
2023-10-31 18:28:59 +0530

    2nd commit

--- src/main/java/midterm/app.java
+++ src/main/java/midterm/app.java
@@ -1,5 +1,12 @@
 package midterm;
 
 public class app {
-
+	private static final String predefinedUsername = 
+			"Susan"; 
+	private static final String predefinedId = 
+			"20MIC0006"; 
+	public static boolean validate(String inputUsername, 
+	String inputId) { 
+	return inputUsername.equals(predefinedUsername) && inputId.equals(predefinedId);
+}
 }
